e5f9bc4f140f23913430c1c9f84db1eebc7439be
Ievgen Kyvgyla <Ievgen_Kyvgyla@epam.com>
2018-10-31 15:06:57 +0300

    Added fluent interface

--- public/index.php
+++ public/index.php
@@ -11,9 +11,9 @@ use Framework\Http\Request;
 chdir(dirname(__DIR__));
 require 'vendor/autoload.php';
 ### Initialization
-$request = new Request();
-$request->withQueryParams($_GET);
-$request->withParsedBody($_POST);
+$request = (new Request())
+  ->withQueryParams($_GET)
+  ->withParsedBody($_POST);
 ### Action
 $name = $request->getQueryParams()['name'] ?? 'Guest';
 
--- src/Framework/Http/Request.php
+++ src/Framework/Http/Request.php
@@ -11,18 +11,25 @@ namespace Framework\Http;
 class Request
 {
 
-    private $queryParams = [];
+    private $queryParams;
 
     private $parsedBody;
+
+    public function __construct(array $queryParams = [], $parsedBody = null)
+    {
+        $this->queryParams = $queryParams;
+        $this->parsedBody = $parsedBody;
+    }
 
     public function getQueryParams(): array
     {
         return $this->queryParams;
     }
 
-    public function withQueryParams(array $query)
+    public function withQueryParams(array $query): self
     {
         $this->queryParams = $query;
+        return $this;
     }
 
     public function getParsedBody()
@@ -30,8 +37,9 @@ class Request
         return $this->parsedBody;
     }
 
-    public function withParsedBody($data)
+    public function withParsedBody($data): self
     {
         $this->parsedBody = $data;
+        return $this;
     }
 }
