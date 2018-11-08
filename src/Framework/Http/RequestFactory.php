2913ffe93923b2503484424724b0853bffd28811
Ievgen Kyvgyla <Ievgen_Kyvgyla@epam.com>
2018-11-08 17:05:04 +0300

    format RequestFactory

--- src/Framework/Http/RequestFactory.php
+++ src/Framework/Http/RequestFactory.php
@@ -12,8 +12,8 @@ class RequestFactory
 {
 
     public static function fromGlobals(
-      array $query = null,
-      array $body = null
+        array $query = null,
+        array $body = null
     ): Request {
         return (new Request())
           ->withQueryParams($query ?: $_GET)
