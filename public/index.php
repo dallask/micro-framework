235a7f3bb4ac9f355798e7cb1accc0ad2e5ce59c
Ievgen Kyvgyla <Ievgen_Kyvgyla@epam.com>
2018-11-09 15:29:11 +0300

    add SapiEmitter

--- public/index.php
+++ public/index.php
@@ -6,8 +6,8 @@
  * Time: 17:57
  */
 
-use Framework\Http\ResponseSender;
 use Zend\Diactoros\Response\HtmlResponse;
+use Zend\Diactoros\Response\SapiEmitter;
 use Zend\Diactoros\ServerRequestFactory;
 
 chdir(dirname(__DIR__));
@@ -20,5 +20,5 @@ $name = $request->getQueryParams()['name'] ?? 'Guest';
 $response = (new HtmlResponse('Hello, ' . $name . '!'))
   ->withHeader('X-Developer', 'ElisDN');
 ### Sending
-$emitter = new ResponseSender();
-$emitter->send($response);
+$emitter = new SapiEmitter();
+$emitter->emit($response);
