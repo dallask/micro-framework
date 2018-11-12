cbdba4648ef255a1d85ee70f11376fbae19babaf
Ievgen Kyvgyla <Ievgen_Kyvgyla@epam.com>
2018-11-12 10:45:20 +0300

    Incapsulate route behavior

--- src/Framework/Http/Router/Route.php
+++ src/Framework/Http/Router/Route.php
@@ -8,18 +8,20 @@
 
 namespace Framework\Http\Router;
 
+use Psr\Http\Message\ServerRequestInterface;
+
 class Route
 {
 
-    public $name;
+    private $name;
 
-    public $pattern;
+    private $pattern;
 
-    public $handler;
+    private $handler;
 
-    public $tokens;
+    private $tokens;
 
-    public $methods;
+    private $methods;
 
     public function __construct(
         $name,
@@ -33,5 +35,51 @@ class Route
         $this->handler = $handler;
         $this->tokens = $tokens;
         $this->methods = $methods;
+    }
+
+    public function match(ServerRequestInterface $request): ?Result
+    {
+        if ($this->methods
+          && !\in_array($request->getMethod(), $this->methods, true)
+        ) {
+            return null;
+        }
+
+        $pattern = preg_replace_callback('~\{([^\}]+)\}~', function ($matches) {
+            $argument = $matches[1];
+            $replace = $this->tokens[$argument] ?? '[^}]+';
+            return '(?P<' . $argument . '>' . $replace . ')';
+        }, $this->pattern);
+        $path = $request->getUri()->getPath();
+
+        if (!preg_match('~^' . $pattern . '$~i', $path, $matches)) {
+            return null;
+        }
+        return new Result(
+            $this->name,
+            $this->handler,
+            array_filter($matches, '\is_string', ARRAY_FILTER_USE_KEY)
+        );
+    }
+
+    public function generate($name, array $params = []): ?string
+    {
+        $arguments = array_filter($params);
+        if ($name !== $this->name) {
+            return null;
+        }
+        $url = preg_replace_callback(
+            '~\{([^\}]+)\}~',
+            function ($matches) use (&$arguments) {
+                $argument = $matches[1];
+                if (!array_key_exists($argument, $arguments)) {
+                    throw new \InvalidArgumentException('Missing parameter "'
+                    . $argument . '"');
+                }
+                return $arguments[$argument];
+            },
+            $this->pattern
+        );
+        return $url;
     }
 }
