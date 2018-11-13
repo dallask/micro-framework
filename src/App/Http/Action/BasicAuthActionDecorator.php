2fb12cebea1226d8aa36816da66dbd80f720407d
Ievgen Kyvgyla <Ievgen_Kyvgyla@epam.com>
2018-11-13 10:55:19 +0300

    Add auth decorator

---
+++ src/App/Http/Action/BasicAuthActionDecorator.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Ievgen_Kyvgyla
+ * Date: 13-Nov-18
+ * Time: 10:49
+ */
+
+namespace App\Http\Action;
+
+use Psr\Http\Message\ServerRequestInterface;
+use Zend\Diactoros\Response\EmptyResponse;
+
+class BasicAuthActionDecorator
+{
+
+    private $next;
+
+    private $users;
+
+    public function __construct(callable $next, array $users)
+    {
+        $this->next = $next;
+        $this->users = $users;
+    }
+
+    public function __invoke(ServerRequestInterface $request)
+    {
+        $username = $request->getServerParams()['PHP_AUTH_USER'] ?? null;
+        $password = $request->getServerParams()['PHP_AUTH_PW'] ?? null;
+        if (!empty($username) && !empty($password)) {
+            foreach ($this->users as $name => $pass) {
+                if ($username === $name && $password === $pass) {
+                    return ($this->next)($request->withAttribute(
+                        'username',
+                        $username
+                    ));
+                }
+            }
+        }
+        return new EmptyResponse(
+            401,
+            ['WWW-Authenticate' => 'Basic realm=Restricted area']
+        );
+    }
+}
