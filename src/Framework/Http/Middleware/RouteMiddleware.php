d5f5943ef48c390c1c82b117d82c28623782fb50
Ievgen Kyvgyla <Ievgen_Kyvgyla@epam.com>
2018-11-15 15:30:29 +0300

    add route middleware

---
+++ src/Framework/Http/Middleware/RouteMiddleware.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Ievgen_Kyvgyla
+ * Date: 15-Nov-18
+ * Time: 15:29
+ */
+
+namespace Framework\Http\Middleware;
+
+use Framework\Http\Pipeline\MiddlewareResolver;
+use Framework\Http\Router\Exception\RequestNotMatchedException;
+use Framework\Http\Router\Router;
+use Psr\Http\Message\ServerRequestInterface;
+
+class RouteMiddleware
+{
+
+    private $router;
+
+    private $resolver;
+
+    public function __construct(Router $router, MiddlewareResolver $resolver)
+    {
+        $this->router = $router;
+        $this->resolver = $resolver;
+    }
+
+    public function __invoke(ServerRequestInterface $request, callable $next)
+    {
+        try {
+            $result = $this->router->match($request);
+            foreach ($result->getAttributes() as $attribute => $value) {
+                $request = $request->withAttribute($attribute, $value);
+            }
+            $middleware = $this->resolver->resolve($result->getHandler());
+            return $middleware($request, $next);
+        } catch (RequestNotMatchedException $e) {
+            return $next($request);
+        }
+    }
+}
