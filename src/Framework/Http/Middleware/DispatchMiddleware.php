476b7bba53a3417e6bd596768bef7fc5d3819862
Ievgen Kyvgyla <Ievgen_Kyvgyla@epam.com>
2018-11-15 15:33:37 +0300

    Separated dispatch middleware

---
+++ src/Framework/Http/Middleware/DispatchMiddleware.php
@@ -0,0 +1,35 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Ievgen_Kyvgyla
+ * Date: 15-Nov-18
+ * Time: 15:31
+ */
+
+namespace Framework\Http\Middleware;
+
+use Framework\Http\Pipeline\MiddlewareResolver;
+use Framework\Http\Router\Result;
+use Psr\Http\Message\ServerRequestInterface;
+
+class DispatchMiddleware
+{
+
+    private $resolver;
+
+    public function __construct(MiddlewareResolver $resolver)
+    {
+        $this->resolver = $resolver;
+    }
+
+    public function __invoke(ServerRequestInterface $request, callable $next)
+    {
+        /** @var Result $result */
+        if (!$result = $request->getAttribute(Result::class)) {
+            return $next($request);
+        }
+        $middleware = $this->resolver->resolve($result->getHandler());
+        return $middleware($request, $next);
+    }
+}
