589dc8edf956267811355465792112e1f171e4be
Ievgen Kyvgyla <Ievgen_Kyvgyla@epam.com>
2018-11-15 16:18:35 +0300

    add single instances support

--- src/Framework/Container/Container.php
+++ src/Framework/Container/Container.php
@@ -13,23 +13,32 @@ class Container
 
     private $definitions = [];
 
+    private $results = [];
+
     public function get($id)
     {
+        if (array_key_exists($id, $this->results)) {
+            return $this->results[$id];
+        }
+
         if (!array_key_exists($id, $this->definitions)) {
             throw new ServiceNotFoundException('Unknown service "' . $id . '"');
         }
 
         $definition = $this->definitions[$id];
         if ($definition instanceof \Closure) {
-            $result = $definition();
+            $this->results[$id] = $definition();
         } else {
-            $result = $definition;
+            $this->results[$id] = $definition;
         }
-        return $result;
+        return $this->results[$id];
     }
 
     public function set($id, $value): void
     {
+        if (array_key_exists($id, $this->results)) {
+            unset($this->results[$id]);
+        }
         $this->definitions[$id] = $value;
     }
 }
