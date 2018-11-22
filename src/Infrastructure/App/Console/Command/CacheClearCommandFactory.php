b8f6843c0e44bc17564d43185046caff529e3f3a
Ievgen Kyvgyla <Ievgen_Kyvgyla@epam.com>
2018-11-22 13:42:56 +0300

    add cache command configuration

---
+++ src/Infrastructure/App/Console/Command/CacheClearCommandFactory.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Ievgen_Kyvgyla
+ * Date: 22-Nov-18
+ * Time: 13:42
+ */
+
+namespace Infrastructure\App\Console\Command;
+
+use App\Console\Command\CacheClearCommand;
+use Psr\Container\ContainerInterface;
+
+class CacheClearCommandFactory
+{
+    public function __invoke(ContainerInterface $container)
+    {
+        return new CacheClearCommand(
+            $container->get('config')['console']['cachePaths']
+        );
+    }
+}
